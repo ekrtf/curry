913a39ad30f9ece20269564cb29b7945f8c2f8b5
zceiekr <emile@kratiroff.com>
2017-03-30 15:49:24 +0100

    (feat) minimal api 2 and 3 in place

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import json
 from flask import Flask, jsonify
-from country_adapter import get_country_currencies
+from country_adapter import get_country_currencies, get_currency_country
 
 app = Flask(__name__)
 
 @app.route('/country/<name>')
-def country_currency(name):
-	curr = get_country_currencies(name)
-	return jsonify(curr)
+def country_to_currency(name):
+	currencies = get_country_currencies(name)
+	return jsonify(currencies)
+
+@app.route('/currency/<country>')
+def currency_to_country(country):
+	countries = get_currency_country(country)
+	return jsonify(countries)
 
 if __name__ == '__main__':
 	app.run()

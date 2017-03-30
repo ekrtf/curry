ef277f035994e2a000cdb0797569ebc2c9c809e7
zceiekr <emile@kratiroff.com>
2017-03-30 15:54:13 +0100

    (feat) basic convert running

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, jsonify
 from country_adapter import get_country_currencies, get_currency_country
+from rate_adapter import get_rate
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def country_to_currency(name):
 def currency_to_country(country):
 	countries = get_currency_country(country)
 	return jsonify(countries)
+
+@app.route('/convert/<source>/<dest>/<amount>')
+def convert(source, dest, amount):
+	rate = get_rate(source, dest)
+	result = float(amount) * float(rate)
+	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run()

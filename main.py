bb45d8378fa59a1c0323e0e38c3ccbf923ad66a9
zceiekr <emile@kratiroff.com>
2017-03-30 14:50:54 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+key = "8ee81402f0f1373c35aac807d814325d"
+
+def get_rate(source, dest):
+	payload = {
+		'access_key': key,
+		'source': source,
+		'currencies': dest
+	}
+	res = requests.get('http://apilayer.net/api/live', params=payload).json()
+	return next(res['quotes'].itervalues())
+
+print get_rate('USD', 'EUR')
